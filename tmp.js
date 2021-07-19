d571c9cfeee9292b308b8fc9cf4d8485ad0b7c69
KBO2001 <64903941+KBO2001@users.noreply.github.com>
2021-07-19 15:58:40 +0900

    Delete tmp.js

--- tmp.js
+++
@@ -1,9 +0,0 @@
-let xhr = new XMLHttpRequest();
-xhr.open('GET', "tmp.json");
-
-xhr.onload = () => {
-  let responseJson = JSON.parse(xhr.response);
-  console.log(responseJson[0][0]);
-}
-
-xhr.send();
