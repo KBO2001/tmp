2218c4c44263baa1f4de1d12bafcbc96f5980163
KBO2001 <64903941+KBO2001@users.noreply.github.com>
2021-07-20 18:08:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ let wordList = [
     [2, `L0,v`, `mean`, `verb`, `change`, `note`, 0, null, null],
     [3, `L0,a`, `mean`, `adj`, `change`, `note`, 0, null, null],
     [4, `L1,n`, `mean`, `noun`, `change`, `note`, 1, null, null],
-    [5, `L1,v`, `mean`, `verb`, `change`, `note`, 1, null, null],
-    [6, `L1,a`, `mean`, `adj`, `change`, `note`, 1, null, null],
-    [7, `L2,n`, `mean`, `noun`, `change`, `note`, 2, null, null],
+    [5, `L1,v`, `mean`, `verb`, ``, `note`, 1, null, null],
+    [6, `L1,a`, `mean`, `adj`, `change`, ``, 1, null, null],
+    [7, `L2,n`, `mean`, `noun`, ``, ``, 2, null, null],
     [8, `L2,v`, `mean`, `verb`, `change`, `note`, 2, null, null]
 ];
 
@@ -58,27 +58,41 @@ function load() {
     btn4.innerHTML = null;
     btn5.innerHTML = `開始`;
     EcardQ.innerHTML = `
-        Please Choose the Lv.<br>
         <form id="rLv">
-            <input type="radio" id = "Lv0" name="rnLv" value="0">Lv.0</input>
-            <input type="radio" id = "Lv1" name="rnLv" value="1">Lv.1</input>
-            <input type="radio" id = "Lv2" name="rnLv" value="2">Lv.2</input>
-            <input type="radio" id = "LvA" name="rnLv" value="A">all</input>
+            <input type="radio" name="rnLv" checked>All</input>
+            <input type="radio" name="rnLv">Lv.0</input>
+            <input type="radio" name="rnLv">Lv.1</input>
+            <input type="radio" name="rnLv">Lv.2</input>
+            <!--
+            <input type="radio" name="rnLv">Lv.3</input>
+            <input type="radio" name="rnLv">Lv.4</input>
+            <input type="radio" name="rnLv">Lv.5</input>
+            -->
         </form>
-        Please Choose the Part.<br>
+        <hr>
         <form id="rPart">
-            <input type="radio" id="noun" name="rnPart" value="noun">noun</input>
-            <input type="radio" id="verb" name="rnPart" value="verb">verb</input>
-            <input type="radio" id="adj" name="rnPart" value="adj">adj</input>
-            <input type="radio" id="PartA" name="rnPart" value="PartA">all</input>
+            <input type="radio" name="rnPart" checked>All</input>
+            <input type="radio" name="rnPart">名詞</input>
+            <input type="radio" name="rnPart">動詞</input>
+            <input type="radio" name="rnPart">副詞</input>
+            <br>
+            <input type="radio" name="rnPart">形容詞</input>
+            <input type="radio" name="rnPart">前置詞</input>
+            <input type="radio" name="rnPart">接続詞</input>
+            <!--
+            <br>
+            <input type="radio" name="rnPart">冠詞</input>
+            <input type="radio" name="rnPart">代名詞</input>
+            <input type="radio" name="rnPart">数詞</input>
+            <input type="radio" name="rnPart">間投詞</input>
+            -->
         </form>`;
     EcardA.innerHTML = `
-        Please Choose the State.<br>
         <form id="rState">
-            <input type="radio" id="Co" name="rnState" value="Co">正解</input>
-            <input type="radio" id="Fa" name="rnState" value="Fa">不正解</input>
-            <input type="radio" id="Un" name="rnState" value="Un">未解答</input>
-            <input type="radio" id="StateA" name="rnState" value="StateA">all</input>
+            <input type="radio" name="rnState" checked>All</input>
+            <input type="radio" name="rnState">正解</input>
+            <input type="radio" name="rnState">不正解</input>
+            <input type="radio" name="rnState">未解答</input>
             </form>`;
     console.log(`onload`);
     console.log(`wordList length: ${wordList.length}`);
@@ -91,37 +105,62 @@ btn5.onclick = function () {
 
 function mode() {
     if (rLv[0].checked == true) {
-        Lv = 0;
-    } else if (rLv[1].checked == true) {
-        Lv = 1;
-    } else if (rLv[2].checked == true) {
-        Lv = 2;
-    } else if (rLv[3].checked == true) {
         Lv = 9;
+    } else if (rLv[1].checked == true) {
+        Lv = 0;
+    } else if (rLv[2].checked == true) {
+        Lv = 1;
+    } else if (rLv[3].checked == true) {
+        Lv = 2;
+/*
+    } else if (rLv[4].checked == true) {
+        Lv = 3;
+    } else if (rLv[5].checked == true) {
+        Lv = 4;
+    } else if (rLv[6].checked == true) {
+        Lv = 5;
+*/
     } else {
         console.log(`Lv is not choosed.`)
         return reload();
     }
+    //0All, 1名詞, 2動詞, 3副詞, 4形容詞, 5前置詞, 6接続詞, 7冠詞, 8代名詞 ,9数詞, 10間投詞
     if (rPart[0].checked == true) {
-        Part = `noun`;
+        Part = 0;
     } else if (rPart[1].checked == true) {
-        Part = `verb`;
+        Part = 1;
     } else if (rPart[2].checked == true) {
-        Part = `adj`;
+        Part = 2;
     } else if (rPart[3].checked == true) {
-        Part = `PartA`;
+        Part = 3;
+    } else if (rPart[4].checked == true) {
+        Part = 4;
+    } else if (rPart[5].checked == true) {
+        Part = 5;
+    } else if (rPart[6].checked == true) {
+        Part = 6;
+/*
+    } else if (rPart[7].checked == true) {
+        Part = 7;
+    } else if (rPart[8].checked == true) {
+        Part = 8;
+    } else if (rPart[9].checked == true) {
+        Part = 9;
+    } else if (rPart[10].checked == true) {
+        Part = 10;
+*/
     } else {
         console.log(`Part is not choosed.`)
         return reload();
     }
     if (rState[0].checked == true) {
-        State = 1;
+        State = 9;
     } else if (rState[1].checked == true) {
-        State = 2;
+        State = 1;
     } else if (rState[2].checked == true) {
-        State = 0;
+        State = 2;
     } else if (rState[3].checked == true) {
-        State = `StateA`;
+        State = 0;
     } else {
         console.log(`State is not choosed.`)
         return reload();
@@ -173,25 +212,70 @@ function chooseLv() {
 }
 
 function choosePart() {
-    if (Part == `noun`) {
+    //0All, 1名詞, 2動詞, 3副詞, 4形容詞, 5前置詞, 6接続詞, 7冠詞, 8代名詞 ,9数詞, 10間投詞
+    if (Part == 1) {
         for (let i = 0; i < chooseList1.length; i++) {
-            if (chooseList1[i][3] == `noun`) {
+            if (chooseList1[i][3] == 1) {
                 chooseList2.push(chooseList1[i]);
             }
         }
-    } else if (Part == `verb`) {
+    } else if (Part == 2) {
         for (let i = 0; i < chooseList1.length; i++) {
-            if (chooseList1[i][3] == `verb`) {
+            if (chooseList1[i][3] == 2) {
                 chooseList2.push(chooseList1[i]);
             }
         }
-    } else if (Part == `adj`) {
+    } else if (Part == 3) {
         for (let i = 0; i < chooseList1.length; i++) {
-            if (chooseList1[i][3] == `adj`) {
+            if (chooseList1[i][3] == 3) {
                 chooseList2.push(chooseList1[i]);
             }
         }
-    } else if (Part == `PartA`) { //all
+    } else if (Part == 4) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 4) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    } else if (Part == 5) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 5) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    } else if (Part == 6) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 6) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    /*
+    } else if (Part == 7) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 7) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    } else if (Part == 8) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 8) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    } else if (Part == 9) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 9) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    } else if (Part == 10) {
+        for (let i = 0; i < chooseList1.length; i++) {
+            if (chooseList1[i][3] == 10) {
+                chooseList2.push(chooseList1[i]);
+            }
+        }
+    */
+    } else if (Part == 0) { //all
         for (let i = 0; i < chooseList1.length; i++) {
             chooseList2.push(chooseList1[i]);
         }
@@ -222,7 +306,7 @@ function chooseState() {
                 chooseList3.push(chooseList2[i]);
             }
         }
-    } else if (State == `StateA`) { //all
+    } else if (State == 9) { //all
         for (let i = 0; i < chooseList2.length; i++) {
             chooseList3.push(chooseList2[i]);
         }
@@ -275,8 +359,12 @@ btn3.onclick = function () {
 function open() {
     Acard1.innerHTML = sortList[now][2];
     Acard2.innerHTML = `品詞： ${sortList[now][3]}`;
-    Acard3.innerHTML = `変化： ${sortList[now][4]}`;
-    Acard4.innerHTML = `備考： ${sortList[now][5]}`;
+    if(!(sortList[now][4] == ``)){
+        Acard3.innerHTML = `変化： ${sortList[now][4]}`;
+    };
+    if(!(sortList[now][5] == ``)){
+        Acard4.innerHTML = `備考： ${sortList[now][5]}`;
+    }
 }
 
 btn0.onclick = function () {
